a33ec5ffcd09c901fdfffdf4e0deb608fae4bf4e
SharlokAlcazar <sharlok.alcazargutierrez@unicolombo.edu.co>
2025-03-19 15:27:17 -0500

    feat: contructor de usuarios y getters and setters

--- src/model/Usuario.java
+++ src/model/Usuario.java
@@ -9,7 +9,52 @@ package model;
  * @author gameV
  */
 public class Usuario {
+    //sharlok
     private String name;
+    private String user;
+    private String password;
+    private String correo;
+
+    public Usuario(String name, String user, String password, String correo) {
+        this.name = name;
+        this.user = user;
+        this.password = password;
+        this.correo = correo;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+    
+    
     
     
 }
